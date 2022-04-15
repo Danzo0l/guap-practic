f6df67a7a30e609a54ec1e1b2111f907fc056185
danzo0l <dryagindenisrus@gmial.com>
2022-04-15 09:56:22 -0700

    15april

--- min_rast.cpp
+++ min_rast.cpp
@@ -2,8 +2,16 @@
 #include <math.h>
 using namespace std;
 
-int arr_len = 5;
-int arr[] = {0x1, 0xaa, 0xaf, 0x12, 0x57};
+int arr_len = 32;
+int arr[] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f};
+
+int reschet(int n) {
+    int result = 0;
+    for (int i=0; i<n; i++) {
+        result += i;
+    }
+    return result;
+}
 
 int count_d(int a) {
     int count = 0;
@@ -25,12 +33,15 @@ int main() {
     int min = 2147483646;
     int i;
     int j;
+    int count = 0;
     for (i=0; i<arr_len; i++) {
         for (j=i+1; j<arr_len; j++) {
             int buf = hemming(arr[i], arr[j]); 
             if (buf < min) {
                 min = buf;
+                
             }
+            count++;
             printf("<%d,%d>", arr[i], arr[j]);
             printf(" %d, ", buf);
             printf("%d, ", count_d(arr[i]));
@@ -39,6 +50,11 @@ int main() {
         }
     }
 
-    printf("%d", min);
+    printf("\nmin >>>%d", min);
+    printf("\ncount >>>%d", count);
+    printf("\ncount >>>%d", reschet(arr_len));
+
+
+    // расчет по формуле арифметической прогресии от 1 до n-1 , где n - количество элементов в массиве
 }
 
